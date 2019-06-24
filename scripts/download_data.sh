add404ce6f59182a9f86182c4466d52ae82cef99
pelillian <plillian@usc.edu>
2019-06-24 11:31:02 -0700

    Download script edit

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -7,5 +7,5 @@ d=$(echo ${d} | tr '[:upper:]' '[:lower:]')
 echo ${d}
 
 mkdir ~/Git/neuroevolved-agents/run/${d}
-scp -i ~/Scripts/neuro.pem ubuntu@${s}:~/Git/neuroevolved-agents/snapshot_iter000\*.h5 ~/Git/neuroevolved-agents/run/${d}
+scp -i ~/Scripts/neuro.pem ubuntu@${s}:~/Git/neuroevolved-agents/snapshot_iter\*.h5 ~/Git/neuroevolved-agents/run/${d}
 scp -i ~/Scripts/neuro.pem ubuntu@${s}:~/user_data.log ~/Git/neuroevolved-agents/run/${d}

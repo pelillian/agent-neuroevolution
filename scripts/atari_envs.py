8f3117a84f711acc4371ce20942aa956f8780b29
Peter Lillian <plillian@usc.edu>
2019-07-23 17:33:16 -0700

    atari envs script

---
+++ scripts/atari_envs.py
@@ -0,0 +1,16 @@
+import gym
+import atari_py as ap
+
+all_envs = gym.envs.registry.all()
+env_ids = [env_spec.id for env_spec in all_envs]
+env_ids = [x for x in env_ids if '-ram' not in x]
+env_ids = [x for x in env_ids if 'Deterministic' not in x]
+env_ids = [x for x in env_ids if 'NoFrameskip' in x]
+
+atari_games = ap.list_games()
+env_ids = [x for x in env_ids if any(game.replace('_', '') in x.lower() for game in atari_games)]
+env_ids = [x for x in env_ids if 'v4' in x]
+print(env_ids)
+
+#print([game for game in atari_games if all(game.replace('_', '') not in x.lower() for x in env_ids)])
+# ['kaboom', 'adventure', 'defender']  # these games are missing from gym

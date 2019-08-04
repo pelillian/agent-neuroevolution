1c779ac20e003e329e27bf123a68e7c4219f26ce
Peter Lillian <plillian@usc.edu>
2019-08-04 14:33:52 -0700

    messing with atari_envs

--- scripts/atari_envs.py
+++ scripts/atari_envs.py
@@ -11,9 +11,11 @@ atari_games = ap.list_games()
 env_ids = [x for x in env_ids if any(game.replace('_', '') in x.lower() for game in atari_games)]
 env_ids = [x for x in env_ids if 'v4' in x]
 
+#env_p = gym.make('PitfallNoFrameskip-v4')
+#import pdb; pdb.set_trace()
 action_meanings = [(x, gym.make(x).unwrapped.get_action_meanings()) for x in env_ids]
 for pair in action_meanings:
     print(pair[0], *pair[1])
 
 #print([game for game in atari_games if all(game.replace('_', '') not in x.lower() for x in env_ids)])
-# ['kaboom', 'adventure', 'defender']  # these games are missing from gym
+# ['kaboom', 'adventure', 'defender']  these games are missing from gym
